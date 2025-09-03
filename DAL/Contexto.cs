b98e31c6c35669be3481d42c9725de98b307f015
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-03 08:57:37 -0400

    Creacion del Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDeJugadores.Models;
+
+namespace RegistroDeJugadores.DAL;
+
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+
+        public DbSet<Jugadores> Jugador { get; set; }
+    }
+
