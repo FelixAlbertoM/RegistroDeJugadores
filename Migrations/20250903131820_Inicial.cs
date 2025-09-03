cdee7b1ae55331fa4d19b0b75572d651c019f797
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-03 10:53:44 -0400

    creacion de la migracion y la DB

---
+++ Migrations/20250903131820_Inicial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroDeJugadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jugador",
+                columns: table => new
+                {
+                    JugadorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Partidas = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugador", x => x.JugadorId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugador_Nombres",
+                table: "Jugador",
+                column: "Nombres",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jugador");
+        }
+    }
+}
