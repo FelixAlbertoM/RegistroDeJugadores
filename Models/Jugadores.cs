adbfcc081b4531f9c9eb9a4e00d27f2cf11348d8
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-03 08:51:35 -0400

    creando el modelo

---
+++ Models/Jugadores.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroDeJugadores.Models
+{
+    [Index(nameof(Nombres), IsUnique = true)]
+    public class Jugadores
+    {
+        [Key]
+        public int JugadorId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombres { get; set; }
+
+
+        [Required(ErrorMessage = "La cantidad de partidas son obligatorias")]
+        public int Partidas { get; set; }
+
+    }
+}
