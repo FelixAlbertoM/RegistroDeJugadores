0e4e4f18b2bbe9d3158829424be1e9d59cf83e24
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-11 13:50:43 -0400

    Creando el DTO (Data Transfer Object) de partida.
    fue sugerido en los issues

--- DTOs/JugadorDTO.cs
+++ DTOs/JugadorDTO.cs
@@ -1,9 +1,9 @@
-﻿namespace RegistroDeJugadores.DTOs
-{
+﻿namespace RegistroDeJugadores.DTOs;
+
     public class JugadorDTO
     {
         public int JugadorId { get; set; }
         public string Nombres { get; set; } = string.Empty;
         public int Partidas { get; set; }
     }
-}
+
---
+++ DTOs/PartidaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RegistroDeJugadores.DTOs;
+
+    public class PartidaDTO
+    {
+        public int PartidaId { get; set; }
+        public string Jugador1 { get; set; } = string.Empty;
+        public string Jugador2 { get; set; } = string.Empty;
+        public string EstadoPartida { get; set; } = string.Empty;
+        public string Ganador { get; set; } = string.Empty;
+    }
+
