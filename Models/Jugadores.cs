4850cfa59401a7557208892536c7c26d158fe138
FelixAlbertoM <af301509@gmail.com>
2025-09-15 20:52:47 -0400

    Actualizar Jugadores.cs
    modificando modelo de jugadores

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -16,7 +16,13 @@ namespace RegistroDeJugadores.Models;
 
 
         [Range(0, int.MaxValue, ErrorMessage = "Las partidas no pueden ser negativas")]
-        public int Partidas { get; set; }
+        public int Victorias { get; set; } = 0;
+
+        public int Derrotas {get; set;} = 0;
+
+        public int Empates {get; set;} = 0;
 
     }
+
+
 
